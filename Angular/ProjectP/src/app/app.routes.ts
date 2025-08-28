55803a90774b485c4698ad6e06cd86cee01a57dd
Yashwanth-Balaji <yashwanthbalaji7264@gmail.com>
2025-08-28 19:39:26 +0530

    send

--- Angular/ProjectP/src/app/app.routes.ts
+++ Angular/ProjectP/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Admin } from '../../component/admin/admin';
+import { User } from '../../component/user/user';
+import { SignalEx } from '../../component/signal-ex/signal-ex';
+import { Databinding } from '../../component/databinding/databinding';
+import { ControlFlow } from '../../component/control-flow/control-flow';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'admin',
+        component:Admin
+    },
+    {
+        path:'user',
+        component:User
+    },
+    {
+        path:'signal',
+        component:SignalEx
+    },
+    {
+        path:'dataBinding',
+        component:Databinding
+    },
+    {
+        path:'control',
+        component:ControlFlow
+    }
+];
--- Angular/ProjectP/src/app/app.ts
+++ Angular/ProjectP/src/app/app.ts
@@ -4,12 +4,13 @@ import { User } from '../../component/user/user';
 import { Databinding } from '../../component/databinding/databinding';
 import { SignalEx } from '../../component/signal-ex/signal-ex';
 import { ControlFlow } from '../../component/control-flow/control-flow';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ControlFlow],        // can import other standalone components too
+  imports: [RouterLink,RouterOutlet],        // can import other standalone components too
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
